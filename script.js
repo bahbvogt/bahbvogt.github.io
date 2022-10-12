d0202f3f480bcd545606433aa70b204efa103d10
bahbvogt <115378957+bahbvogt@users.noreply.github.com>
2022-10-11 20:34:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const links = document.getElementsByTagName('a');
 
-const diacritics = ["&#772;", "&#781;", "&#782;", "&#809;", "&#840;", "&#851;", "&#838;", "&#810;", "&#826;", "&#827;", "&#817;", "&#794;", "&#841;", "&#792;", "&#793;", "&#797;", "&#798;", "&#799;", "&#812;", "&#813;", "&#770;", "&#780;"];
+const diacritics = ["&#851;", "&#867;", "&#868;", "&#869;", "&#870;", "&#871;", "&#872;", "&#873;", "&#874;", "&#875;", "&#876;", "&#877;", "&#878;"];
 
 for(const link of links){
 	if(link.parentElement.nodeName.toLowerCase() != "banner") {
 		link.addEventListener('mouseover', function handleMouseOver() {
 		var startStr = link.innerHTML;
 		var startLength = link.innerHTML.length;
-		for (var i = startLength; i > 0; i = i - 1) {
+		for (var i = startLength; i >= 0; i = i - 1) {
 			if(startStr.charAt(i-1) != " "){
 				var add = "";
 				for(var j = 0; j < Math.floor(Math.random() * 9) + 3; j++){
-					add += diacritics[Math.floor(Math.random() * 22)];
+					add += diacritics[Math.floor(Math.random() * 13)];
 				}
 				link.innerHTML = link.innerHTML.slice(0, i) + add + link.innerHTML.slice(i);
 			}
@@ -19,7 +19,7 @@ for(const link of links){
 	});
 
 	link.addEventListener('mouseout', function handleMouseOut() {
-		link.innerHTML = link.innerHTML.replace(/[^\w\s\.\-&'@<>]/g, '');
+		link.innerHTML = link.innerHTML.replace(/[^\w\s\.\-&<>]/g, '');
 	});
 	}
 }
